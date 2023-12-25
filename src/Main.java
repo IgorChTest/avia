a3e359e02511952dad9fb7871f109469c82d5395
IgorCH QA <ic641716@gmail.com>
2023-12-25 09:47:42 +0300

    Changed discriptions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int y = 16_281;
-        int z = 20;
-        int x = (y / z);
+        int y = 16_281 //Стоимость билета;
+        int z = 20; //Количество потраченных рублей для одной мили
+        int x = (y / z); //Количество начисленных бонусных миль
         System.out.println(x);
     }
 }
